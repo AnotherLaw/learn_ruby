24613da82a57da9d355f5a7e3cc67fb793fff650
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-12 19:03:13 +1100

    Add titling function

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -14,3 +14,19 @@ end
 def start_of_word(str, num)
     return str[0..num-1]
 end
+
+def first_word(sentence)
+    return sentence.split()[0]
+end
+
+def titleize(sentence)
+    words = sentence.split.map   do |word|
+        if %w(the and over).include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+    words.first.capitalize!
+    words.join(" ")
+end
