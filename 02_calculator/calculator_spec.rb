a4b48971b4ec274ef17dd7b5033da3a5b85ccb5b
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-12 17:27:40 +1100

    Add multiply test

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,9 +79,14 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+    it "multiplies two numbers" do
+        expect(multiply[2,5]).to eq(10)
+    end
+        
 
-  it "multiplies several numbers"
+    it "multiplies several numbers" do
+        expect(multiply[2,3,8]).to eq(48)
+    end
   
 end
 
