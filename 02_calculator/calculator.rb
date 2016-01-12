765b7cd47e934f7c324f3c587d44f09a16394e44
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-12 17:14:37 +1100

    Add function for add and subtract

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,10 @@
+# calculator.rb
+# Has a series of functions that calculate a bunch of stuff.
+
+def add(first_number, second_number)
+    return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+    return first_number - second_number
+end
