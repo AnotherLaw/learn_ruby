c43f752b00885843cb8a654db3f92ac1f8756cc1
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-12 17:24:19 +1100

    Set sum function up

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,10 +1,21 @@
 # calculator.rb
 # Has a series of functions that calculate a bunch of stuff.
-
+# These first two functions (add, subtract) are self explanatory.
 def add(first_number, second_number)
     return first_number + second_number
 end
 
 def subtract(first_number, second_number)
     return first_number - second_number
+end
+
+# sum however must be able to add all numbers in an array, regardless of size.
+def sum(nums_to_sum)
+    total = 0
+    i = 0
+    while i < nums_to_sum.length
+        total = total + nums_to_sum[i]
+        i += 1
+    end
+    return total
 end
