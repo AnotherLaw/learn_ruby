be8293beed5d969898e8fddf5ff87917d263f8dc
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-12 17:06:09 +1100

    Add formula

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,9 +1,9 @@
 # I just realised what this thing was supposed to do...
 
-def ftoc
-    
+def ftoc(fheit)
+    return (fheit.to_f - 32) * 5 / 9
 end
 
-def ctof
-
+def ctof(celc)
+    return celc.to_f * 9 / 5 + 32
 end
